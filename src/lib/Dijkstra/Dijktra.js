b88b11780440a4ac5e128442486ce4a9986f5ad0
Herbin Clément <leever@hotmail.fr>
2021-11-13 17:06:36 +0100

    implements dijkstra algorithm

--- src/lib/Dijkstra/Dijktra.js
+++ src/lib/Dijkstra/Dijktra.js
@@ -1,4 +1,4 @@
-const extractMinValue = (distances) => {
+const extractMinValue = (distances, vertices) => {
     let dmin = Infinity;
     let vmin = null;
     for (const [v, d] of distances.entries()) {
@@ -7,9 +7,29 @@ const extractMinValue = (distances) => {
             dmin = d;
         }
     }
+    distances.delete(vmin);
+    vertices = vertices.filter((v) => v !== vmin);
     return {vmin, dmin};
 }
 
-const Dijkstra = (G, start_vertice) => {
-
+const initialiseDistance = (start_vertice, vertices) => {
+    const distances = new Map();
+    for (const v of vertices) {
+        distances.set(v, Infinity);
+    }
+    distances.set(start_vertice, 0);
+    return distances;
 }
+
+const Dijkstra = (G, start_vertice) => {
+    const vertices = G.get_vertices();
+    const distances = initialiseDistance(start_vertice, vertices);
+    while (distances.size !== 0) {
+        const {vmin, dmin} = extractMinValue(distances, vertices)
+        for (const neighbor of G.neighbors(vmin)) {
+            distances.set(v, Math.min(distances.get(v), dmin + 1));
+        }
+    }
+    return distances;
+}
+
